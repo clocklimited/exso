cace9f8e747d021e8a9526ad566a6d0213ada683
Ash Summers <ash.summers@clock.co.uk>
2024-02-15 22:32:21 +0000

    feat(add-to-release): fail on invalid branch name

--- actions/release-management/lib/add-to-release.js
+++ actions/release-management/lib/add-to-release.js
@@ -9,17 +9,23 @@ function createAddToRelease (serviceLocator) {
     , addToExistingRelease = createExistingReleaseAdder(serviceLocator)
 
   function addToRelease (pr, comment, releaseNameNumber, skipStatusChecks, cb) {
+    var commentToAdd = null
     // don't run on release PRs
     if (pr.branch.indexOf('release/') === 0) return cb()
+    // make sure branches start with the right names, otherwise releases will/can break
+    if (!['feature/', 'feat/', 'bug/', 'fix/'].some(prefix => pr.branch.includes(prefix))) {
+      commentToAdd = '@' + comment.author + ' Pull Request branches must start with `feature/`, `feat/`, ' +
+        '`bug/` or `fix/`. Please recreate this Pull Request with a valid branch name.'
+      return pr.addComment(commentToAdd, cb)
+    }
     if (pr.baseRef !== 'master') {
-      var commentToAdd = '@' + comment.author + ' Only Pull Requests based off of `master` ' +
+      commentToAdd = '@' + comment.author + ' Only Pull Requests based off of `master` ' +
         'can be merged into to a release. Please merge this Pull Request and then add the base branch to the release.'
       return pr.addComment(commentToAdd, cb)
     }
     pr.getCurrentStatus(function (error, status) {
       if (error) return cb(error)
-      var commentToAdd = null
-        , repoManager = null
+      var repoManager = null
 
       if (!skipStatusChecks && status.state !== 'success' && status.statuses.length > 0) {
         commentToAdd = '@' + comment.author + ' Not all status checks are passing.' +
